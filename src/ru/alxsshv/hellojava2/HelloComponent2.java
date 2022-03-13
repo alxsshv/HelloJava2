2806a89b53353dbc8c59c03ffbb21822b3aea312
alxsshv <alxsshv@gmail.com>
2022-03-13 22:41:22 +0300

    HJ3

--- src/ru/alxsshv/hellojava2/HelloComponent2.java
+++ src/ru/alxsshv/hellojava2/HelloComponent2.java
@@ -7,14 +7,33 @@ import javax.swing.*;
 import java.awt.event.*;
 
 class HelloComponent2 extends JComponent
-implements MouseMotionListener 
+implements MouseMotionListener, ActionListener 
 {
 String theMessage;
 
 int messageX = 125, messageY = 95 ;//координаты сообщения
 
+JButton theButton;
+
+int colorIndex; // текущий указатель в объекте someColors
+
+static Color[] someColors = {
+		
+		Color.black, Color.red, Color.white, Color.green, Color.blue, Color.magenta
+}; // массив цветов
+
 public HelloComponent2 (String message) {
+	
 	theMessage = message;
+	
+	theButton = new JButton ("Change color");
+	
+	setLayout (new FlowLayout() );
+	
+	add(theButton );
+	
+	theButton.addActionListener(this);
+	
 	addMouseMotionListener (this);
 }	
 public void paintComponent (Graphics g) {
@@ -34,5 +53,31 @@ public void mouseDragged (MouseEvent e) {
 public void mouseMoved (MouseEvent e) {
 	
 }
+public void actionPerformed (ActionEvent e) {
+	// кто-то нажимал нашу кнопку
+	
+	if  (e.getSource() == theButton) {
+		
+		changeColor();
+		
+	}
+}
+
+synchronized private Color currentColor(  ) {
+    return someColors[colorIndex];
+  }
+
+synchronized private void  changeColor () {
+	
+	if (++colorIndex == someColors.length) 
+		
+		colorIndex = 0;
+	
+		setForeground ( currentColor() ); // использовать новый цвет
+	
+		repaint ();
+}
+	
+	
 
 }
